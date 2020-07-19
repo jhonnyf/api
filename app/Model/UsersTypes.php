56f081491eb11b841f14cb834c90f73af71abb2c
Jhonny Fialho <jhonny@hello-world.cc>
2020-07-19 01:34:50 -0300

    ajuste

--- app/Model/UsersTypes.php
+++ app/Model/UsersTypes.php
@@ -7,5 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 class UsersTypes extends Model
 {
     protected $primaryKey = 'id';
-    protected $fillable = ['user_type'];
+    protected $fillable   = ['user_type'];
+
 }
